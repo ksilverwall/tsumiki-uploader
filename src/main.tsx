d4ad2177133d3d698ebe74a5d9204ecae6ec3107
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-07-03 13:42:38 +0900

    Display error on init

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Router as RemixRouter } from "@remix-run/router";
 import App from "./App.tsx";
 import "./index.css";
 import { CreateConnector } from "./builder.ts";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App connector={CreateConnector()} />,
-  },
-]);
+type ApplicationConstants = {
+  router: RemixRouter;
+  error?: undefined;
+} | {
+  error: Error;
+}
+
+function init(): ApplicationConstants {
+  try {
+    const connector = CreateConnector()
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <App connector={connector} />,
+      },
+    ]);
+
+    return { router: router };
+  } catch (err) {
+    if (err instanceof Error) {
+      return { error: err };
+    }
+    throw err;
+  }
+}
+
+const constants = init();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {constants.error ? <p>{constants.error.message}</p> : <RouterProvider router={constants.router} />}
   </React.StrictMode>
 );
